fb433e550625f9c6ddbecc8eb67a290d532b5486
NageshJoy007 <Nagesh.joy007@gmail.com>
2019-09-07 17:58:06 +0530

    Adding tests exported from postman

---
+++ README.md
@@ -0,0 +1,4 @@
+# API Testing using Postman & Newman
+
+This is a sample project to run your api tests on your local and also on cicd.
+
---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash "$PWD"
+
+DOCKER_IMAGE=postman/newman_ubuntu1404
+
+docker pull $DOCKER_IMAGE
+
+docker run -t -v "$PWD":/usr/src -w /usr/src --entrypoint '/bin/sh' $DOCKER_IMAGE -c "npm link newman-reporter-html && newman run sample-api.postman_collection.json -e sample-api-env.postman_environment.json -r html"
+
+
+
