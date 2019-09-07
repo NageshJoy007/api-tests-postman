88f843f422d26e1e7521880abd9edd4c41d59da2
NageshJoy007 <Nagesh.joy007@gmail.com>
2019-09-07 22:07:25 +0530

    Adding htmlextra reports

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ DOCKER_IMAGE=postman/newman_ubuntu1404
 
 docker pull $DOCKER_IMAGE
 
-docker run -t -v "$PWD":/usr/src -w /usr/src --entrypoint '/bin/sh' $DOCKER_IMAGE -c "npm link newman-reporter-html && newman run sample-api.postman_collection.json -e sample-api-env.postman_environment.json -r html"
-
+docker run -t -v "$PWD":/usr/src -w /usr/src --entrypoint '/bin/sh' $DOCKER_IMAGE -c "npm link newman-reporter-htmlextra && newman run sample-api.postman_collection.json -e sample-api-env.postman_environment.json -r htmlextra"
 
 
